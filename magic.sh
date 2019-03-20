ec721e20b696bf80a4c1df57929dba7f421e6ab3
Anton Holovin <aholovi@softserveinc.com>
2019-03-20 19:37:54 +0200

    modified:   magic.sh

--- magic.sh
+++ magic.sh
@@ -18,12 +18,11 @@ set CRUMB=$(cat crumb.txt)
 while ! [ -s $(pwd)/crumb.txt ]
 do
 curl 'http://admin:'$JENKINS_PASSWORD'@'$JENKINSHOST':8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' > crumb.txt
-set CRUMB=$(cat crumb.txt)
+cat crumb.txt
+set CRUMB=$(head -n 1 $(pwd)/crumb.txt)
 echo $CRUMB
 sleep 1
 done
-set CRUMB=$(curl 'http://admin:'$JENKINS_PASSWORD'@'$JENKINSHOST':8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-rm crumb.txt
 echo "Your crumb is $CRUMB"
 echo "Please, insert your GIT username":
 read GIT_USER
